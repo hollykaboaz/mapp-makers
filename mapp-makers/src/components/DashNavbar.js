8b2d053679781e9e2fe261831577a4c80bcff83b
Duaa Fatima <duaa.fatima44@gmail.com>
2023-11-01 13:30:18 -0400

    Develop course header component and make changes in dashboard accordingly.

--- mapp-makers/src/components/DashNavbar.js
+++ mapp-makers/src/components/DashNavbar.js
@@ -9,20 +9,20 @@ export const DashNavbar = () => {
   };
   return (
     <div className= "items-center pr-5 border-b-[0.5px] border-b-stone-400 border-solid">
-      <div className='flex w-[323px] max-w-full items-start justify-between gap-5 mt-2.5 max-md:justify-center' >
+      <div className='flex w-[323px] max-w-full items-start justify-between gap-5 mt-2.5 max-md:justify-center pl-5' >
      
       <div 
       className={` text-base self-stretch ${selectedItem === 'Classes' ? 'text-black border-b-2 border-green-500' : 'text-gray-400'} 
-                ${selectedItem !== 'Classes'? 'hover:text-slate-500 hover:text-lg': ''}`}
+                ${selectedItem !== 'Classes'? 'hover:text-slate-500 hover:text-lg hover:border-b-2 hover:border-green-500': ''}`}
       onClick= {() => handleItemClick('Classes')}
       > Classes</div>
       <div
       className={` text-base self-stretch ${selectedItem === 'Students' ? 'text-black border-b-2 border-green-500' : 'text-gray-400'}
-                ${selectedItem !== 'Students'? 'hover:text-slate-500 hover:text-lg': ''}`}
+                ${selectedItem !== 'Students'? 'hover:text-slate-500 hover:text-lg hover:border-b-2 hover:border-green-500': ''}`}
       onClick= {() => handleItemClick('Students')}
       > Students</div>
       <div className={` text-base self-stretch ${selectedItem === 'Attendance' ? 'text-black border-b-2 border-green-500' : 'text-gray-400'}
-                 ${selectedItem !== 'Attendance'? 'hover:text-slate-500 hover:text-lg': ''}`}
+                 ${selectedItem !== 'Attendance'? 'hover:text-slate-500 hover:text-lg hover:border-b-2 hover:border-green-500': ''}`}
       onClick= {() => handleItemClick('Attendance')}
       > Attendance</div>
       </div>

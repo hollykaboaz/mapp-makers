e866c73db08eb5fab780d188728485913d37f78b
hollykaboaz <hollykaboaz@gmail.com>
2023-11-01 18:15:22 -0400

    created sidebar

--- mapp-makers/src/index.js
+++ mapp-makers/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import App from './components/App'
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import SignUpForm from './pages/Sign Up/SignUpForm';
 import {AuthProvider} from "./firebase/AuthContext";
 import {SignInLayout} from "./layouts/SignInLayout";
-import SignInForm from "./pages/Sign In/SignInForm"; // Import the AuthProvider
+import SignInForm from "./pages/Sign In/SignInForm";
+import Dashboard from "./components/Dashboard"; // Import the AuthProvider
 
 
 
@@ -24,6 +24,12 @@ root.render(
                         <SignInLayout page='signIn'>
                           <SignInForm/>
                         </SignInLayout>
+                    }/>
+                    <Route path='/dashboard' element={
+
+                        <Dashboard></Dashboard>
+
+
                     }/>
                 </Routes>
             </BrowserRouter>

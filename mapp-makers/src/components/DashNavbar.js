60f8e4b96698af1bb873e54d15ffb161d9aa7107
hollykaboaz <hollykaboaz@gmail.com>
2023-11-09 00:59:42 -0500

    added add student button

--- mapp-makers/src/components/DashNavbar.js
+++ mapp-makers/src/components/DashNavbar.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 
 export const DashNavbar = () => {
@@ -11,13 +12,23 @@ export const DashNavbar = () => {
 
     return (
 
-        <div className="navbar font-light">
-            <a className = { selectedItem === 'Classes' ? 'font-medium nav-link nav-link-grow-up' : 'nav-link nav-link-grow-up'}
-               onClick= {() => handleItemClick('Classes')}  href="#">Classes</a>
-            <a  className = { selectedItem === 'Students' ? 'font-medium nav-link nav-link-grow-up' : 'nav-link nav-link-grow-up'} onClick= {() => handleItemClick('Students')} href="#">Students</a>
-            <a  className = { selectedItem === 'Attendance' ? 'font-medium nav-link nav-link-grow-up' : 'nav-link nav-link-grow-up'}
-                onClick= {() => handleItemClick('Attendance')} href="#">Attendance</a>
+        <div className='flex flex-row justify-between navbar'>
+            <div className=" font-light">
+                <a className = { selectedItem === 'Classes' ? 'font-medium nav-link nav-link-grow-up' : 'nav-link nav-link-grow-up'}
+                   onClick= {() => handleItemClick('Classes')}  href="#">Classes</a>
+                <a  className = { selectedItem === 'Students' ? 'font-medium nav-link nav-link-grow-up' : 'nav-link nav-link-grow-up'} onClick= {() => handleItemClick('Students')} href="#">Students</a>
+                <a  className = { selectedItem === 'Attendance' ? 'font-medium nav-link nav-link-grow-up' : 'nav-link nav-link-grow-up'}
+                    onClick= {() => handleItemClick('Attendance')} href="#">Attendance</a>
+            </div>
+
+            <a href='#' className='flex flex-row gap-2 rounded text-white text-sm p-2 bg-black items-center mb-2'>
+                <p>Add Student</p>
+                <a href="">
+                    <FontAwesomeIcon icon="fa-solid fa-plus"/>
+                </a>
+            </a>
         </div>
+
 
 
         // <div className="mt-2 items-center border-b border-gray-200 border-solid">

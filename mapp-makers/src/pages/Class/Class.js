c9a2f451d4a933c9b3a267ef18c69dff766177e0
hollykaboaz <hollykaboaz@gmail.com>
2023-11-26 15:37:47 -0500

    clean up qr code

--- mapp-makers/src/pages/Class/Class.js
+++ mapp-makers/src/pages/Class/Class.js
@@ -12,10 +12,8 @@ function Class(props) {
     useEffect(() => {
         const scanner = new Html5QrcodeScanner('reader', {
             qrbox: {
-                width: 500,
-                height: 500
-            },
-            fps: 5
+                width: 350, height: 250
+            }, fps: 5
         })
 
         scanner.render(success, error);
@@ -29,49 +27,44 @@ function Class(props) {
         }
     }, []);
 
-    const data = [
-        {
-            id: 1,
-            image: sImage,
-            lastName: 'Khawaja',
-            firstName: 'Duaa',
-            status: 'Present',
-            in: '3:00pm'
-        }
+    const data = [{
+        id: 1, image: sImage, lastName: 'Khawaja', firstName: 'Duaa', status: 'Present', in: '3:00pm'
+    }
 
     ]
 
     return (
         <div className=' py-4'>
+            <div className='grid grid-cols-3 gap-2'>
+                <div className='flex flex-col gap-2'>
+                    <div className=' h-fit flex flex-col text-green-900 bg-green-200 px-4 py-4 rounded-3xl'>
+                        <FontAwesomeIcon className='h-6 w-6 pb-6' icon="fa-solid fa-user-group"/>
+                        <div className='text-5xl font-semibold'>23</div>
+                        <div className='font-light'>Present</div>
+                    </div>
 
-
-            <div className='grid grid-rows-1 grid-cols-3 gap-16'>
-
-                <div className='flex flex-col text-green-900 bg-green-200 px-4 py-4 rounded-3xl'>
-                    <FontAwesomeIcon className='h-6 w-6 pb-6' icon="fa-solid fa-user-group"/>
-                    <div className='text-5xl font-semibold'>23</div>
-                    <div className='font-light'>Present</div>
+                    <div className=' h-fit flex flex-col text-red-900 bg-red-200 px-4 py-4 rounded-3xl'>
+                        <FontAwesomeIcon className='h-6 w-6 pb-6' icon="fa-solid fa-user-xmark"/>
+                        <div className='text-5xl font-semibold'>5</div>
+                        <div className='font-light'>Absent</div>
+                    </div>
                 </div>
 
-                <div className='flex flex-col text-red-900 bg-red-200 px-4 py-4 rounded-3xl'>
-                    <FontAwesomeIcon className='h-6 w-6 pb-6' icon="fa-solid fa-user-xmark"/>
-                    <div className='text-5xl font-semibold'>5</div>
-                    <div className='font-light'>Absent</div>
-                </div>
-
-                <div className='flex flex-col text-gray-900 bg-gray-200 px-4 py-4 rounded-3xl'>
+                <div className='col-span-2 row-span-1  flex flex-col text-gray-900 bg-gray-200 px-4 py-4 rounded-3xl'>
                     <FontAwesomeIcon className='h-6 w-6 pb-6' icon="fa-solid fa-qrcode"/>
                     <div className='font-light'>Scan in Students</div>
-
                     <div className=''>
-                        {scanResult ? <div> Success: <a className = 'text-green-500' href={scanResult}>{scanResult}</a></div> : <div id="reader"></div>}
+                        {scanResult ?
+                            <div> Success: <a className='text-green-500' href={scanResult}>{scanResult}</a></div> :
+                            <div id="reader"></div>}
                     </div>
                 </div>
             </div>
+
+
             <div className='text-2xl font-medium mt-12'> Attendance Report</div>
             <Table data={data}></Table>
-        </div>
-    );
+        </div>);
 }
 
 export default Class;
